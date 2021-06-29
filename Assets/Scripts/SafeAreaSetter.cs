0eb6ccf9f361efea3815699049768bee6f6b21e9
eungyukim <eungyukm@gmail.com>
2021-06-29 15:47:12 +0900

    Add SafeAreaSetter

---
+++ Assets/Scripts/SafeAreaSetter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeAreaSetter : MonoBehaviour
+{
+    public Canvas canvas;
+
+    private RectTransform panelSafeArea;
+    private Rect currentSafeArea = new Rect();
+    private ScreenOrientation currentOrientation = ScreenOrientation.AutoRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelSafeArea = GetComponent<RectTransform>();
+
+        currentOrientation = Screen.orientation;
+        currentSafeArea = Screen.safeArea;
+
+        ApplaySafeArea();
+    }
+
+    private void ApplaySafeArea()
+    {
+        if (panelSafeArea == null)
+            return;
+
+        Rect safeArea = Screen.safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= canvas.pixelRect.width;
+        anchorMin.y /= canvas.pixelRect.height;
+
+        anchorMax.x /= canvas.pixelRect.width;
+        anchorMax.y /= canvas.pixelRect.height;
+
+        panelSafeArea.anchorMin = anchorMin;
+        panelSafeArea.anchorMax = anchorMax;
+
+        currentOrientation = Screen.orientation;
+        currentSafeArea = Screen.safeArea;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((currentOrientation != Screen.orientation) || (currentSafeArea != Screen.safeArea))
+        {
+            ApplaySafeArea();
+        }
+    }
+}
